7cc7839ca399d59873b4d515d4bf4dac7bfb7ac8
tomas-paskovskij <tomas.paskovskij@gmail.com>
2026-02-16 18:21:40 +0200

    feat: created dev socket url though tunnnel

--- frontend/src/services/socket.ts
+++ frontend/src/services/socket.ts
@@ -1,8 +1,11 @@
 import { io } from "socket.io-client";
 
-// Jei tavo backend veikia kitu portu, pakeisk čia
-const SOCKET_URL = "http://localhost:3000";
+const SOCKET_URL =
+  window.location.hostname === "localhost"
+    ? "http://localhost:3000"
+    : "https://ms7w8bsm-3000.euw.devtunnels.ms";
 
 export const socket = io(SOCKET_URL, {
-  autoConnect: false, // Mes prisijungsime rankiniu būdu, kai turėsime vardą
+  transports: ["websocket"],
+  autoConnect: true,
 });
