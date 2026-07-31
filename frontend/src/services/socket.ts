66ddb0b83f8429a0794e23ec832e82efae4263f6
tomas-paskovskij <tomas.paskovskij@gmail.com>
2026-07-31 15:05:27 +0300

    2.0 - MVP veikiantis žaidimas

--- frontend/src/services/socket.ts
+++ frontend/src/services/socket.ts
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 const SOCKET_URL =
   window.location.hostname === "localhost"
     ? "http://localhost:3000"
-    : "https://nkr84w11-3000.euw.devtunnels.ms";
+    : "https://5b1spzmn-3000.euw.devtunnels.ms";
 
 export const socket = io(SOCKET_URL, {
   transports: ["websocket", "polling"], // Polling padeda, jei websocket blokuojamas
